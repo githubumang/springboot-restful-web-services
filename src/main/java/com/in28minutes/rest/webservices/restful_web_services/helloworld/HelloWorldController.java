1cb639fb144d27c5cff6be19462fb55872d2646e
UMANG AGRAWAL <umangagrawal@192.168.1.10>
2025-07-22 18:59:22 +0530

    practice rest api

---
+++ src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -0,0 +1,28 @@
+package com.in28minutes.rest.webservices.restful_web_services.helloworld;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean getMethodName() {
+        return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping("/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World %s", name));
+    }
+    
+    
+}
