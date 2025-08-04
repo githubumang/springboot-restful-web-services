f1b6881c76814b46e4a86edf885f53d969a308af
UMANG AGRAWAL <umangagrawal@192.168.1.7>
2025-08-04 16:29:54 +0530

    setup h2 database and jap repository

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO user_details(id, birth_date, name)
+VALUES(10000, current_date(), 'Umang');
+
+insert into user_details(id,birth_date,name)
+values(10001, current_date(), 'Ranga');
+
+insert into user_details(id,birth_date,name)
+values(10002, current_date(), 'Ravi');
+
+insert into user_details(id,birth_date,name)
+values(10003, current_date(), 'Sathish');
